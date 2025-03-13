a5103e38905bc4b91694ab280ad7ff681748e2a2
Guilherme <guilherme.carlos@55pbx.com>
2025-03-13 19:05:33 -0300

    continuando

--- src/components/questions/question.tsx
+++ src/components/questions/question.tsx
@@ -1,22 +1,29 @@
 // Question
-
-import React from "react";
-
 type question = {
-    question: string,
+    question: any,
     userAnswer: string,
     handleAnswer: string
 }
 
 function Question({ question, userAnswer, handleAnswer }: question) {
+    const flag = question.rightAnswer?.flag
+    const options = question?.allOptions
+    const arr: any[] = []
+
+    function randomArrayIndexPicker(obj: any) {
+        for (let key in obj) {
+            arr.push(obj[Math.ceil(Math.random() * arr.length - 1)])
+        };
+        console.log(Math.ceil(Math.random() * (arr.length - 1)))
+    }
+    randomArrayIndexPicker(options)
     return <div className="question">
-        <div>Pergunta {question}</div>
-        <div>
-            <div>opcao 1</div>
-            <div>opcao 2</div>
-            <div>opcao 3</div>
-            <div>opcao 4</div>
-        </div>
+        <div>Qual o nome do pais dessa bandeira oh seu comedia?: {flag}</div>
+        {/* <div>
+            {randomArrayIndexPicker(options).map((name) => {
+                return <p>{name}</p>
+            })}
+        </div> */}
     </div>;
 }
 
--- src/components/quiz/quiz.tsx
+++ src/components/quiz/quiz.tsx
@@ -3,25 +3,44 @@ import React, { useState, useEffect } from "react";
 import Question from "../questions/question";
 
 function Quiz() {
-    const [questions, setQuestions] = useState([]);
+    const [questions, setQuestions] = useState<any>([]);
     const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [userAnswers, setUserAnswers] = useState([]);
+    const [userAnswers, setUserAnswers] = useState<any>([]);
     const [showResult, setShowResult] = useState(false);
 
-    async function fetchData() {
-        try {
-            const apiData = await axios.get("https://restcountries.com/v3.1/all?fields=name,flag")
-            const result = apiData.data;
-            console.log(result)
-            return result;
-        } catch (err) {
-            console.log(err);
-        };
-    };
 
-    fetchData();
     useEffect(() => {
-        console.log("oi")
+        const fetchData = async () => {
+            try {
+                const apiData = await axios.get("https://restcountries.com/v3.1/all?fields=name,flag");
+                const result = apiData.data;
+                const randomCountry = Math.ceil(Math.random() * result.length);
+
+                let count = 0;
+                let allOptions: any[] = []
+
+                const rightAnswer = result[randomCountry];
+                allOptions.push(rightAnswer.name.common);
+
+                result.filter((country: any) => {
+                    if (country && count < 3) {
+                        allOptions.push(country.name.common)
+                        count++
+                    }
+                });
+
+                const questionsData = {
+                    rightAnswer,
+                    allOptions
+                }
+                setQuestions(questionsData)
+                return result;
+            } catch (err) {
+                console.log(err);
+            };
+        };
+
+        fetchData();
     }, []);
 
     const handleAnswer = () => {
@@ -41,7 +60,7 @@ function Quiz() {
     };
 
     return <div className="quiz">
-        <Question />
+        <Question question={questions} userAnswer={""} handleAnswer={""} />
     </div>;
 }
 
